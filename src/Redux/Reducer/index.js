596981eb358bc15033087e40b50c694c1e7cf323
chenjiaobin <1066628577@qq.com>
2018-08-09 19:12:09 +0800

    修改

--- src/Redux/Reducer/index.js
+++ src/Redux/Reducer/index.js
@@ -1,9 +1,18 @@
 import { Increment, Decrement } from '../Action/index'
 
+/**
+ * state是旧的state，也就是改变之前的state值
+ * action也是旧的action
+ */
 export default (state, action) => {
+    console.log(state)
+    console.log(action)
     const { counterCaption } = action
     switch (action.type) {
         case Increment:
+            // return的时候不要去改变第一个参数的值，也就是不能这样使用Object.assign(state,[counterCaption]:state[counterCaption]+1)
+            // 但是你可以通过赋值到一个新的对象Object.assign({},state,[counterCaption]:state[counterCaption]+1),这样就不会改变state的值了
+            // 也可以通过下面这种写法，es6的对象展开符
             return {...state, [counterCaption]:state[counterCaption]+1}
         case Decrement:
             return {...state, [counterCaption]:state[counterCaption]+1}
--- src/components/counter.js
+++ src/components/counter.js
@@ -11,6 +11,10 @@ function Counter ({caption, Increment, Decrement, value}) {
         </div>
     )
 }
+/**
+ * 用于建立store的state的映射关系，可以传入两个参数，必须返回一个对象，会订阅store的状态的变化，每次的store的state发生变化的时候都会被调用
+ * 第二个参数代表本组件的props（也可以不传），如果有传递这个值，那么props发什么变化的时候这个函数会被重新执行
+ */
 
 function mapState (state, ownProps) {
     return {
@@ -28,5 +32,11 @@ function mapDispath (dispath, ownProps) {
         }
     }
 }
-
+/**
+ * connect（参数的顺序不要颠倒了）
+ * 第一个参数将store中的数据作为props绑定到组件上面
+ * 第二个参数将action作为props绑定到组件上面
+ * 第三个参数用于自定义merge流程，将stateProps 和 dispatchProps merge 到parentProps之后赋给组件。通常情况下，你可以不传这个参数，connect会使用 Object.assign。
+ * 第四个参数如果指定这个参数，可以定制 connector 的行为。一般不用。
+ */
 export default connect(mapState, mapDispath)(Counter)
