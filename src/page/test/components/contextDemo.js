972da3f1a48d086456ceb1c4d5c77631ab803464
chenjiaobin <chenjb@keketour.com>
2019-05-17 18:59:06 +0800

    添加测试

--- src/page/test/components/contextDemo.js
+++ src/page/test/components/contextDemo.js
@@ -6,6 +6,14 @@ export default class ContexDemo extends Component {
   constructor() {
     super()
   }
+  componentDidMount () {
+    console.log('季度杀回房间')
+    console.log(this.context)
+  }
+  // 可以通过class.contextType(这个属性名字必须是这个)属性被重新赋值为一个ThemeContextTP创建的context对象，这样我们可以直接通过this.context来消费Context的值
+  // 可以在任何生命周期中访问他，包括render
+  static contextType = ThemeContextTP
+  
   render() {
     return (
       <div>
