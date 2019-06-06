eb83feba1019328739091216cf4990043633e9c7
chenjiaobin <chenjb@keketour.com>
2019-06-06 18:33:28 +0800

    添加学习内容

---
+++ README_reactRedux.md
@@ -0,0 +1,6 @@
+### 学习文档
+https://github.com/react-guide/redux-tutorial-cn
+
+### 经验之谈
+* action的type可以统一用一个文件来管理，这样可以保证命名的统一性，reducer也方便使用这些type值
+* reducer不一定要使用switch
--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -13,6 +13,7 @@ const initState = {
     }
 }
 
+// 所做的只是生成一个函数，这个函数来调用你的一系列 reducer，每个 reducer 根据它们的 key 来筛选出 state 中的一部分数据并处理，然后这个生成的函数再将所有 reducer 的结果合并成一个大的对象
 // combieReducer是合并多个reducer，它会 实施一次安全检查，用以确保reducer永远不会返回undefined，所以在reducer的参数state最好是给个默认值，请参考reducer/reducer_2.js文件
 var reducer = combineReducers({
     [REDUCER_1]: reducer1,
@@ -23,6 +24,21 @@ var reducer = combineReducers({
 //     num_1: {},
 //     num_2: []
 // }
+
+// 也可以不用设置key值
+// var reducer = combineReducers({
+//     reducer1,
+//     reducer2
+// })
+
+// 上面这个相当于
+// function reducer (state = {}, action) {
+//     return {
+//         reducer1: reducer1(state.reducer1, action),
+//         reducer2: reducer1(state.reducer2, action),
+//     }
+// }
+
 
 const store = createStore(reducer, initState)
 
